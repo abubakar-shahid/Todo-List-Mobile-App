0f57f6aad6f88b6af57981f21397986b0c204701
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-10-08 15:50:34 +0500

    Added Java Files

--- app/src/main/java/com/example/assignment_2/MainActivity.java
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -1,24 +1,78 @@
 package com.example.assignment_2;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements TaskInputFragment.OnTaskAddedListener {
+
+    private static final String TAG = "MainActivity";
+    private List<Task> taskList;
+    private TaskAdapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        taskList = new ArrayList<>();
+        RecyclerView taskRecyclerView = findViewById(R.id.taskRecyclerView);
+        taskAdapter = new TaskAdapter(taskList);
+        taskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskRecyclerView.setAdapter(taskAdapter);
+
+        FloatingActionButton fabAddTask = findViewById(R.id.fabAddTask);
+        fabAddTask.setOnClickListener(v -> openTaskInputFragment());
+
+        Log.d(TAG, "onCreate");
+    }
+
+    private void openTaskInputFragment() {
+        TaskInputFragment taskInputFragment = new TaskInputFragment();
+        taskInputFragment.show(getSupportFragmentManager(), "TaskInputFragment");
+    }
+
+    @Override
+    public void onTaskAdded(Task task) {
+        taskList.add(task);
+        taskAdapter.notifyItemInserted(taskList.size() - 1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
